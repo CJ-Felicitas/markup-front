a277ce3e147610ca44609c2b0a03b48eee0ad1ed
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-06-17 19:02:08 +0800

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
         Hello Nuxt Github Firebase Actions 2nd attempt
-        remove the fucking vercel from the repo
+        remove the fucking vercel from the repo 2nd attempt
     </div>
 </template>

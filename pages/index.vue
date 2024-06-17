e7c6e1b96050c8d20f8d62f5f3c6432dc3b62382
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-06-17 20:38:16 +0800

    test merge

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,7 @@
     <div>
         Hello Nuxt Github Firebase Actions 2nd attempt
         remove the fucking vercel from the repo 2nd attempt
+
+        Hello Github Actions
     </div>
 </template>
